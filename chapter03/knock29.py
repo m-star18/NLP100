6c97fa5be4b601fb7a1b6312c41d3d036875b78e
m-star18 <ryuuchei@ezweb.ne.jp>
2020-06-12 17:21:46 +0900

    solve29

---
+++ chapter03/knock29.py
@@ -0,0 +1,45 @@
+import re
+import requests
+import pandas as pd
+
+df = pd.read_json('jawiki-country.json', lines=True)
+text = df.query('title=="イギリス"')['text'].values[0]
+text = text.replace("''", '').split('\n')
+
+memo, flag = [], False
+template = '基礎情報'
+check = re.compile('\|(.+?)\s=\s(.+)')
+check1 = re.compile('\{\{' + template)
+check2 = re.compile('\}\}')
+check3 = re.compile('\|')
+check4 = re.compile('<ref(\s|>).+?(</ref>|$)')
+
+for t in text:
+    if flag:
+        if check2.match(t):
+            break
+        if check3.match(t):
+            memo.append(check4.sub('', t.strip()))
+    if check1.match(t):
+        flag = True
+
+ans = {}
+for tmp in [check.match(m) for m in memo]:
+    if tmp:
+        ans[tmp.group(1)] = tmp.group(2)
+
+r1 = re.compile("'+")
+r2 = re.compile('\[\[(.+\||)(.+?)\]\]')
+r3 = re.compile('\{\{(.+\||)(.+?)\}\}')
+r4 = re.compile('<\s*?/*?\s*?br\s*?/*?\s*>')
+
+for k, v in ans.items():
+    v = r1.sub('', v)
+    v = r2.sub(r'\2', v)
+    v = r3.sub(r'\2', v)
+    v = r4.sub('', v)
+    ans[k] = v
+
+url = 'https://commons.wikimedia.org/w/api.php?action=query&titles=File:' + ans['国旗画像'].replace(' ', '_') + '&prop=imageinfo&iiprop=url&format=json'
+data = requests.get(url)
+print(re.search(r'"url":"(.+?)"', data.text).group(1))
